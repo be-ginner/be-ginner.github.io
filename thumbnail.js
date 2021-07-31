449aab9fd1fe89776f5cc01cdb27671b0af93a6b
be-ginner <87918696+be-ginner@users.noreply.github.com>
2021-08-01 00:11:00 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -4,5 +4,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     targetImage.addEventListener("click", function() {
     alert("点击了图片");
+    targetImage.classList.remove("small");
 });
 });
